f627d14c438c5a51e8ea9eacc78c26f39724db14
Gareth Smith <gsmith@pivotal.io>
2016-01-22 17:57:16 +0000

    Make unprivileged containers really work
     - Maps container-root to not-host-root uid
     - Introduces a small hack to pre-make the .pivot_root directory in the rootfs while creating the bundle.
     This is to work around a bug in our aufs driver which means the root of the rootfs is owned by host-root
     rather than container root.
    
    Signed-off-by: George Lestaris <glestaris@pivotal.io>
    Signed-off-by: Julian Friedman <julz.friedman@uk.ibm.com>

--- rundmc/bundler.go
+++ rundmc/bundler.go
@@ -1,12 +1,7 @@
 package rundmc
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/cloudfoundry-incubator/garden"
 	"github.com/cloudfoundry-incubator/goci"
-	"github.com/cloudfoundry-incubator/goci/specs"
 	"github.com/cloudfoundry-incubator/guardian/gardener"
 )
 
@@ -27,61 +22,4 @@ func (b BundleTemplate) Generate(spec gardener.DesiredContainerSpec) *goci.Bndl 
 	}
 
 	return bndl
-}
-
-type BaseTemplateRule struct {
-	PrivilegedBase   *goci.Bndl
-	UnprivilegedBase *goci.Bndl
-}
-
-func (r BaseTemplateRule) Apply(bndl *goci.Bndl, spec gardener.DesiredContainerSpec) *goci.Bndl {
-	if spec.Privileged {
-		return r.PrivilegedBase
-	} else {
-		return r.UnprivilegedBase
-	}
-}
-
-type RootFSRule struct{}
-
-func (r RootFSRule) Apply(bndl *goci.Bndl, spec gardener.DesiredContainerSpec) *goci.Bndl {
-	return bndl.WithRootFS(spec.RootFSPath)
-}
-
-type NetworkHookRule struct {
-	LogFilePattern string
-}
-
-func (r NetworkHookRule) Apply(bndl *goci.Bndl, spec gardener.DesiredContainerSpec) *goci.Bndl {
-	return bndl.WithPrestartHooks(specs.Hook{
-		Env: []string{fmt.Sprintf(
-			"GARDEN_LOG_FILE="+r.LogFilePattern, spec.Handle),
-			"PATH=" + os.Getenv("PATH"),
-		},
-		Path: spec.NetworkHook.Path,
-		Args: spec.NetworkHook.Args,
-	})
-}
-
-type BindMountsRule struct {
-}
-
-func (b BindMountsRule) Apply(bndl *goci.Bndl, spec gardener.DesiredContainerSpec) *goci.Bndl {
-	var mounts []goci.Mount
-	for _, m := range spec.BindMounts {
-		modeOpt := "ro"
-		if m.Mode == garden.BindMountModeRW {
-			modeOpt = "rw"
-		}
-
-		mounts = append(mounts, goci.Mount{
-			Name:        m.DstPath,
-			Destination: m.DstPath,
-			Source:      m.SrcPath,
-			Type:        "bind",
-			Options:     []string{"bind", modeOpt},
-		})
-	}
-
-	return bndl.WithMounts(mounts...)
 }
