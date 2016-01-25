0c0ec2f138c2c918ae2e530af576eb22238babe5
Julian Friedman <julz.friedman@uk.ibm.com>
2016-01-25 17:08:29 +0000

    Use non-conflicting networkPool in test
    avoids test pollution and means the test actually tests something

--- gqt/net_test.go
+++ gqt/net_test.go
@@ -184,7 +184,7 @@ var _ = Describe("Net", func() {
 			)
 
 			BeforeEach(func() {
-				args = []string{"-networkPool", "10.254.0.0/29"}
+				args = []string{"-networkPool", "10.253.0.0/29"}
 				containerNetwork = ""
 			})
 
@@ -210,6 +210,10 @@ var _ = Describe("Net", func() {
 				Expect(newIpAddress).To(Equal(otherContainerIP))
 			})
 
+			It("vends IPs from the given network pool", func() {
+				Expect(containerIP(otherContainer)).To(ContainSubstring("10.253."))
+			})
+
 			It("is accessible from the outside", func() {
 				hostPort, containerPort, err := otherContainer.NetIn(0, 4321)
 				Expect(err).ToNot(HaveOccurred())
@@ -223,7 +227,7 @@ var _ = Describe("Net", func() {
 				Eventually(func() int {
 					session := sendRequest(externalIP, hostPort)
 					return session.Wait().ExitCode()
-				}, "5s", "1s").Should(Equal(0))
+				}).Should(Equal(0))
 			})
 		})
 
